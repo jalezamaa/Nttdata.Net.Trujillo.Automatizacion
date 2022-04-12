88e1a8e141098ddc938fdcaa35cd26c8576275e0
aojedama <103447397+aojedama@users.noreply.github.com>
2022-04-12 09:58:45 -0500

    Automatizacion v1

---
+++ Automatizacion/ListaDeVariablesTipoListaResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automatizacion
+{
+    public class ListaDeVariablesTipoListaResponse
+    {
+        public string nombreVariable { get; set; }
+        public string tipoVariable { get; set; }
+    }
+}
---
+++ Automatizacion/bin/Debug/claseRequest.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Claro.SIACU.Web.Application.BonusFullClaro.Areas.Bonus.Models.BonusActivation
+{
+  public class claseRequest
+  {
+      public String dato1quest { get; set; } 
+      public Int da222d { get; set; } 
+  }
+}
+
